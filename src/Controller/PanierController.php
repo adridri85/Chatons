59f0517cbb6b8747082fa9263df16906d928e0fd
adrien Poiroux <adrien26poiroux@gmail.com>
2020-09-09 17:47:27 +0200

    add cat on panier

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Panier;
+use App\Repository\CatRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Psr\Container\ContainerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 
@@ -24,5 +29,25 @@ class PanierController extends AbstractController
         return $this->render("panier/panier.html.twig");
     }
 
-    
+
+    /**
+     * @Route("/new/{id}", name="addCat")
+     */
+    public function getInsertCatsOnPannier(EntityManagerInterface $entityManager, Request $request,CatRepository $catRepository,$id){
+
+        $cat = $catRepository->find(array('id' => $id ));
+
+        $panier = new Panier();
+
+        $panier->setAccount($this->getUser());
+        $panier->addCat($cat);
+
+        $entityManager->persist($panier);
+        $entityManager->flush();
+
+
+        return $this->redirectToRoute("panier_view");
+
+
+    }
 }
